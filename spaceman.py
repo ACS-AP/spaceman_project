65ef84341bc4d80afc7be54d510949288a06edf9
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-09-30 09:24:06 -0700

    Updated spaceman function

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,7 @@
 import random
 
+guessed_secret_word = []
+
 def load_word():
     '''
     A function that reads a text file of words and randomly selects one to use as the secret word
@@ -104,6 +106,43 @@ def spaceman(secret_word):
 
     '''
 
+    missedGuesses = 0
+    guesses = 0
+    guessed_letters = []
+
+    print("----------------- WELCOME TO SPACEMAN --------------------")
+    print("Welcome to Spaceman. A new and epic guessing game.")
+    print('')
+    print("--------------------- INSTRUCTIONS -----------------------")
+    print("How it works is simple. There is a word you are trying to guess.")
+    print("You will be guessing letters that you think are in the secret word")
+    print("If you guess a letter in the word, it will fill in the blank.")
+    print("You will have 7 guess attempts to figure out the secret word of you will lose.")
+    print("")
+    print("------------------- LETS GET STARTED --------------------")
+    print("Are you read to start the game!")
+    startGame = input("Are you ready to start the game [Y/N]: ")
+
+    if startGame == "N":
+        print("Thanks for playing. Hope to see you soon.")
+    if startGame == "Y":
+        get_guessed_word(secret_word, guessed_letters)
+        while missedGuesses < 7:
+            guessesLeft = 7 - missedGuesses
+            print(guessed_secret_word)
+            guessed_letter = input(F'You have {guessesLeft} guesses left! Guess a letter: ')
+            guessed_letters.append(guessed_letter)
+            guessedCorrectly = is_guess_in_word(guessed_letter, secret_word)
+            if guessedCorrectly == False:
+                missedGuesses += 1
+            if guessedCorrectly == True:
+                missedGuesses += 0
+            correctlyGuessedWord = is_word_guessed(secret_word, guessed_letters)
+            if correctlyGuessedWord == True:
+                print("Congrats on guessing the word")
+                break
+        if missedGuesses == 7:
+            print("Sorry you ran out of guesses. Better luck next time!")
 
     #TODO: show the player information about the game according to the project spec
 
